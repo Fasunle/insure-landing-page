ac9775a9d28b1a5052aa370c2407cd92be213952
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-11-18 09:17:13 +0100

    feat: footer component

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,11 @@ import bgPatternHowWeWorkMobile from './assets/bg-pattern-how-we-work-mobile.svg
 import snappyProcessIcon from './assets/icon-snappy-process.svg';
 import affordableIcon from './assets/icon-affordable-prices.svg';
 import peopleFirstIcon from './assets/icon-people-first.svg';
+import logo from './assets/logo.svg';
+import facebookIcon from './assets/icon-facebook.svg';
+import twitterIcon from './assets/icon-twitter.svg';
+import instagramIcon from './assets/icon-instagram.svg';
+import pinterestIcon from './assets/icon-pinterest.svg';
 
 function App() {
   return (
@@ -116,8 +121,61 @@ function App() {
           <img src={bgPatternHowWeWorkMobile} alt='' />
         </picture>
       </section>
-      Why Insure? View plans Reviews Help me FAQ Terms of use Privacy policy
-      Cookies Contact Sales Support Live chat Others Careers Press Licenses
+
+      <footer className='footer'>
+        <div className='footer--header'>
+          <div className='imageContainer'>
+            <img className='icon' src={logo} alt='insure logo' />
+          </div>
+          <div className='socials'>
+            <img src={facebookIcon} alt='' className='icon' />
+            <img src={twitterIcon} alt='' className='icon' />
+            <img src={pinterestIcon} alt='' className='icon' />
+            <img src={instagramIcon} alt='' className='icon' />
+          </div>
+        </div>
+
+        <div className='information'>
+          <div className='our-company'>
+            <a href='#/' className='title'>
+              Our Company
+            </a>
+            <a href='#/'>How we work</a>
+            <a href='#/'>Why Insure?</a>
+            <a href='#/'>View plans</a>
+            <a href='#/'>Reviews</a>
+          </div>
+
+          <div className='help-me'>
+            <a href='#/' className='title'>
+              Help me
+            </a>
+            <a href='#/'>FAQ</a>
+            <a href='#/'>Terms of use</a>
+            <a href='#/'>Privacy</a>
+            <a href='#/'>policy</a>
+            <a href='#/'>Cookies</a>
+          </div>
+
+          <div className='contact'>
+            <a href='#/' className='title'>
+              Contact
+            </a>
+            <a href='#/'>Sales</a>
+            <a href='#/'>Support</a>
+            <a href='#/'>Live chat</a>
+          </div>
+
+          <div className='others'>
+            <a href='#/' className='title'>
+              Others
+            </a>
+            <a href='#/'>Careers</a>
+            <a href='#/'>Press</a>
+            <a href='#/'>Licenses</a>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
